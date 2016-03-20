35d004f1cbd55d15d8980a9c02983550e1f46e68
harbanik <harbani.kaur@mail.utoronto.ca>
2016-03-20 18:42:39 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,54 +6,37 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Home_for_Paws
+ * @package _s
  */
 
 ?>
-wordpress
-
-<?php
-/**
- * The footer template file.
- * @package MaidenHair
- * @since MaidenHair 1.0.0
-*/
-?>
-  </div> <!-- end of main-content -->
-  </div> <!-- end of wrapper-content -->
-<footer id="wrapper-footer">
-<?php if ( is_active_sidebar( 'sidebar-2' ) || is_active_sidebar( 'sidebar-3' ) || is_active_sidebar( 'sidebar-4' ) ) { ?>
-  <div id="footer">
-<?php if ( !is_page_template('template-landing-page.php') ) { ?>
-    <div class="footer-widget-area footer-widget-area-1">
-<?php dynamic_sidebar( 'sidebar-2' ); ?>
-    </div>    
-    <div class="footer-widget-area footer-widget-area-2">
-<?php dynamic_sidebar( 'sidebar-3' ); ?>
-    </div>   
-    <div class="footer-widget-area footer-widget-area-3">
-<?php dynamic_sidebar( 'sidebar-4' ); ?>
-    </div>
-<?php } ?>
-  </div>
-<?php } ?>
-<?php if ( dynamic_sidebar( 'sidebar-5' ) ) : else : ?>
-<?php endif; ?>
-</footer>  <!-- end of wrapper-footer -->
-</div> <!-- end of container-shadow -->
-</div> <!-- end of container -->
-<?php wp_footer(); ?>     
-</body>
-</html>
 
 	</div><!-- #content -->
+<div id="footer-menu">
+<?php wp_nav_menu( array( 'theme_location'=>
+'secondary', 'menu_class'=>'foot-menu' ) ); ?>
+</div><!-- #footer-menu -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+	        <div id="footer-widgets">
+	            <?php if ( is_active_sidebar( 'footer' ) ) : ?>
+	                <aside id="widget-foot" class="widget-foot">
+	                    <?php dynamic_sidebar( 'footer' ); ?>
+	                </aside>
+	            <?php endif; ?>
+	        </div><!-- end #footer-widgets -->
+	        
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'home-for-paws' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'home-for-paws' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'home-for-paws' ), 'home-for-paws', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>. All Rights Reserved.</p> 
+                <p class="credits"><?php printf( __( 'Theme: %1$s by %2$s.', 'Harbani and Ash and Ali' ), 'homeforpaws', '<a href="http://underscores.me" rel="designer">Home For Paws</a>' ); ?></p>
 		</div><!-- .site-info -->
+			
+<div id="footer-menu">
+	<?php wp_nav_menu( array( 'theme_location'=>'
+	secondary','menu_class'=>'foot-menu' ) ); ?>
+		</div><!-- #footer-menu -->
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
@@ -61,3 +44,5 @@ wordpress
 
 </body>
 </html>
+
+
